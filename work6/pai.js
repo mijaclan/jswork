1299f995e04f404bcbb9b5350d6bd9d2822589ef
mijaclan556 <2734473891@qq.com>
2019-10-16 00:49:32 +0800

    work6

--- work6/pai.js
+++ work6/pai.js
@@ -6,11 +6,11 @@
   //"计算"按钮点击处理函数
     function calculate(){
     //读取阶数输入框的数值
-    let num = document . getElementById ( 'num' ).value 
-    num = parseInt(num)&&Number(num)
+     let num = document.getElementById ('num').value 
+       num = parseInt(num)&&Number(num)
     if(!isNaN(num)){
     //设置圆周率输入框的值
-        document.getElementById('unm').value  = pi (num) 
+        document.getElementById('pai').value  = pi(num) 
       } 
     }
     //声明阶乘函数n!   factorial()
@@ -19,8 +19,8 @@
        let sum = 1
     //for循环累乘
 
-      for(let i=1;i<=49 ;++i){
-     s
+      for(let i=1;i<=n;++i){
+       sum *=i
       }
         //返回阶乘汇总数
     
@@ -32,8 +32,8 @@
       
         let sum = 1
     //for循环累乘
-        for(let i=1;;++i){
-     s
+        for(let i=1;i<=n;++i){
+     sum *=   2*i+1
         }
     //返回阶乘汇总数
        return sum
@@ -46,13 +46,14 @@
     //for循环计算圆周率
        for(let i=0;i<=num;++i){
     //声明阶乘被除数 dividend,并调用factorial函数取值
-    let dividend = factorial( )
+       let dividend = factorial(i)
     //声明阶乘被除数 divisor,并调用oddFactorial函数取值
-    let divisor = oddFactorial ( ）
+       let divisor = oddFactorial(i)
     //累加汇总值
-   s
-       }
+     sum += (dividend/divisor)
+    //  console.long(sum)
+    }
     //返回圆周率
-    return st 2
+     return sum*2
     }
     calculate()
